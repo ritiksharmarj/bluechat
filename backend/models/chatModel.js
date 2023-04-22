809280771a1f16fc97451417432e845c8b48e7e8
ritiksharmarj <theonlyritik@gmail.com>
2023-04-22 21:27:42 +0530

    Setting up schemas

---
+++ backend/models/chatModel.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+// All the values single chat contains:
+// chatName
+// isGroupChat
+// list of "users"
+// latestMessage
+// If it is group chat then who is "groupAdmin"
+
+const chatSchema = mongoose.Schema(
+  {
+    chatName: {
+      type: String,
+      trim: true,
+    },
+    isGroupChat: {
+      type: Boolean,
+      default: false,
+    },
+    users: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    latestMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Message',
+    },
+    groupAdmin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Chat = mongoose.model('Chat', chatSchema);
--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,16 +1,19 @@
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const ChatPage = () => {
+  const [chats, setChats] = useState([]);
+
   useEffect(() => {
     const fetchChats = async () => {
       const { data } = await axios.get('http://localhost:5000/api/chat');
+      setChats(data);
       console.log(data);
     };
     fetchChats();
   }, []);
 
-  return <div>ChatPage</div>;
+  return <div>{chats[0].chatName}</div>;
 };
 
 export default ChatPage;
