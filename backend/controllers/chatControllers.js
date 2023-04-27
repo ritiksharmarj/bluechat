d4c6148c49eccfd83f831e9512ef541fd664a02f
ritiksharmarj <theonlyritik@gmail.com>
2023-04-28 00:28:13 +0530

    Create group chat api

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -77,3 +77,42 @@ export const fetchChats = asyncHandler(async (req, res) => {
     throw new Error(error.message);
   }
 });
+
+/**
+ * @description - Create new group chat
+ * @route - POST /api/chat/group
+ * @access - Protected
+ */
+export const createGroupChat = asyncHandler(async (req, res) => {
+  if (!req.body.users || !req.body.name) {
+    return res.status(400).send({ message: 'Please Fill all the fields!' });
+  }
+
+  let users = JSON.parse(req.body.users);
+
+  if (users.length < 2) {
+    return res
+      .status(400)
+      .send('More than 2 users are required to create a group chat');
+  }
+
+  users.push(req.user);
+
+  try {
+    const groupChat = await Chat.create({
+      chatName: req.body.name,
+      users: users,
+      isGroupChat: true,
+      groupAdmin: req.user,
+    });
+
+    const fullGroupChat = await Chat.findOne({ _id: groupChat._id })
+      .populate('users', '-password')
+      .populate('groupAdmin', '-password');
+
+    res.status(200).json(fullGroupChat);
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
