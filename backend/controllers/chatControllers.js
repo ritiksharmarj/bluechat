f956741b91447dc518932adeeb55663aace6a31c
ritiksharmarj <theonlyritik@gmail.com>
2023-04-28 00:11:48 +0530

    fetch users chat api

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -52,3 +52,28 @@ export const accessChat = asyncHandler(async (req, res) => {
     }
   }
 });
+
+/**
+ * @description - Fetch all chats for a user
+ * @route - GET /api/chat/
+ * @access - Protected
+ */
+export const fetchChats = asyncHandler(async (req, res) => {
+  try {
+    Chat.find({ users: { $elemMatch: { $eq: req.user._id } } })
+      .populate('users', '-password')
+      .populate('groupAdmin', '-password')
+      .populate('latestMessage')
+      .sort({ updatedAt: -1 })
+      .then(async (results) => {
+        results = await User.populate(results, {
+          path: 'latestMessage.sender',
+          select: 'name avatar email',
+        });
+        res.status(200).send(results);
+      });
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
